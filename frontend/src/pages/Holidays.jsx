342fde0fd91ca40922b14c41dc1472ef4e40a01e
Tirth-esh <tirthesh10.aggarwal@gmail.com>
2024-06-12 18:07:22 +0530

    Holiday page added

---
+++ frontend/src/pages/Holidays.jsx
@@ -0,0 +1,120 @@
+import '../css/Holidays.css';
+const Holidays = () => {
+    return (
+        <div>
+            <h1>Holiday dates for the University</h1>
+            <h3>*Booking shall be closed on the following dates</h3>
+
+           <div className="holiday_table_container">
+           <table className="holiday_table">
+                <tr>
+                    <th>Holiday</th>
+                    <th>Date</th>
+                </tr>
+    <tr>
+        <td>Republic Day</td>
+        <td>26th January</td>
+    </tr>
+    <tr>
+        <td>Shivratri</td>
+        <td>8th March</td>
+    </tr>
+    <tr>
+        <td>Holi</td>
+        <td>25th March</td>
+    </tr>
+    <tr>
+        <td>Good Friday</td>
+        <td>29th March</td>
+    </tr>
+    <tr>
+        <td>Eid-Ul- Fitr</td>
+        <td>11th April</td>
+    </tr>
+    <tr>
+        <td>Baisakhi</td>
+        <td>13th April</td>
+    </tr>
+    <tr>
+        <td>Ram Navami</td>
+        <td>17th April</td>
+    </tr>
+    <tr>
+        <td>Mahavir Jayanti</td>
+        <td>21st April</td>
+    </tr>
+    <tr>
+        <td>Buddha Purnima</td>
+        <td>23rd May</td>
+    </tr>
+    <tr>
+        <td>Bakrid</td>
+        <td>17th June</td>
+    </tr>
+    <tr>
+        <td>Muharram</td>
+        <td>17th July</td>
+    </tr>
+    <tr>
+        <td>Independence Day</td>
+        <td>15th August</td>
+    </tr>
+    <tr>
+        <td>Raksha Bandhan</td>
+        <td>19th August</td>
+    </tr>
+    <tr>
+        <td>Janmashtami</td>
+        <td>26th August</td>
+    </tr>
+    <tr>
+        <td>Ganesh Chaturthi</td>
+        <td>7th September</td>
+    </tr>
+    <tr>
+        <td>Onam</td>
+        <td>15th September</td>
+    </tr>
+    <tr>
+        <td>Milad-un-nabi</td>
+        <td>16th September</td>
+    </tr>
+    <tr>
+        <td>Gandhi Jayanti</td>
+        <td>2nd October</td>
+    </tr>
+    <tr>
+        <td>Dussehra</td>
+        <td>12th October</td>
+    </tr>
+    <tr>
+        <td>Maharishi Valmiki’s Birthday</td>
+        <td>17th October</td>
+    </tr>
+    <tr>
+        <td>Diwali</td>
+        <td>31st October</td>
+    </tr>
+    <tr>
+        <td>Bhai Dooj</td>
+        <td>3rd November</td>
+    </tr>
+    <tr>
+        <td>Chhat Puja</td>
+        <td>7th November</td>
+    </tr>
+    <tr>
+        <td>Guru Nanak Jayanti</td>
+        <td>15th November</td>
+    </tr>
+    <tr>
+        <td>Christmas Day</td>
+        <td>25th December</td>
+    </tr>
+            </table>
+           </div>
+        </div>
+    );
+};
+
+export default Holidays;
