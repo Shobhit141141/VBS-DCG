3ace76605f74992589939cc5e42bccb49128bbb8
Aman Jain <amanjain0006@gmail.com>
2024-06-18 18:05:46 +0530

    navbar changes

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -1,9 +1,9 @@
 import { Link } from 'react-router-dom';
 import { FaBars } from 'react-icons/fa'
-import { RiLogoutCircleRLine } from "react-icons/ri";
 import { useNavigate } from 'react-router-dom';
 import '../css/Nav.css';
 import { useState } from 'react';
+import { IoCloseSharp } from "react-icons/io5";
 
 function Nav() {
   const [menu, setMenu] = useState(false);
@@ -16,32 +16,30 @@ function Nav() {
   const handleLogout = () => {
     localStorage.removeItem('token');
     navigate('/login');
+    setMenu(false);
+  };
+  const handleLinkClick = () => {
+    setMenu(false); // Close the menu when a link is clicked
   };
 
   return (
     <div id={`navbar ${menu ? '_active' : ''}`} className='menubar-comp'>
       <div id='menubar'>
-        {!menu ? (
-          <div className='nav_toggle' onClick={handleToggle}>
-            <FaBars />
-          </div>
-        ) : (
-          <div className='nav_toggle' onClick={handleToggle}>
-            <FaBars />
-          </div>
-        )}
+          <div className={`nav_toggle ${menu ? 'open' : ''}`} onClick={handleToggle}>
+          <FaBars />
+        </div>
       </div>
       <div className={`nav_list${menu ? '_active' : ''} `}>
-        <Link to='/' className='nav_home'>
+        <Link to='/' className='nav_home' onClick={handleLinkClick}>
           <h4>Home</h4>
         </Link>
-        <Link to='/Book' className='nav_book'>
+        <Link to='/Book' className='nav_book'  onClick={handleLinkClick}>
           <h4>Book</h4>
         </Link>
-        <Link to='/Contact' className='nav_contact'>
+        <Link to='/Contact' className='nav_contact'  onClick={handleLinkClick}>
           <h4>Contact Us</h4>
         </Link>
-        <Link to='/Holidays' className='Holidays'>
+        <Link to='/Holidays' className='Holidays'  onClick={handleLinkClick}>
           <h4>Holidays</h4>
         </Link>
         {!localStorage.getItem('token') ? (
@@ -49,14 +47,14 @@ function Nav() {
             <h4>Signup</h4>
           </Link>
         ) : (
-          <Link to='/my-slots'>My slots</Link>
+          <Link to='/my-slots'  onClick={handleLinkClick}>My slots</Link>
         )}
         {!localStorage.getItem('token') ? (
           <Link to='/Login' className='nav_login'>
             <h4>Login</h4>
           </Link>
         ) : (
-          <RiLogoutCircleRLine className='nav_logout' onClick={handleLogout} /> 
+          <IoCloseSharp className='nav_logout' onClick={handleLogout} /> 
         )}
       </div>
     </div>
