3088e889595419099ea371725ec0c98389501f62
Shobhit Tiwari <122107079+Shobhit141141@users.noreply.github.com>
2024-06-22 18:03:43 +0530

    chore: Refactor multerConfig.js to create upload directory if it doesn't exist

--- backend/config/multerConfig.js
+++ backend/config/multerConfig.js
@@ -1,26 +1,8 @@
 const multer = require('multer');
-const fs = require('fs');
-const path = require('path');
-
-// Define the directory path
-const uploadDir = path.join(__dirname, '..', 'public', 'uploads');
-
-// Ensure the public/uploads directory exists
-if (!fs.existsSync(uploadDir)) {
-  try {
-    fs.mkdirSync(uploadDir, { recursive: true });
-    console.log('Upload directory created successfully.');
-  } catch (err) {
-    console.error('Error creating upload directory:', err);
-    // Handle error appropriately, such as logging or throwing an exception
-    // Note: You may want to throw the error to propagate it back to the calling code
-    throw err;
-  }
-}
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, uploadDir); // Folder where the images will be stored
+    cb(null, 'public/uploads/'); // Folder where the images will be stored
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + '-' + file.originalname);
@@ -28,5 +10,4 @@ const storage = multer.diskStorage({
 });
 
 const uploadImg = multer({ storage: storage });
-
 module.exports = { uploadImg };
